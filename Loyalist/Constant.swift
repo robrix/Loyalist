165ac5f65afed7dd81950cbc2217aa7cbb84c269
Rob Rix <rob.rix@github.com>
2014-08-01 21:37:30 -0400

    Constants are convertible from Int/Float literals.

--- Loyalist/Constant.swift
+++ Loyalist/Constant.swift
@@ -3,3 +3,15 @@
 struct Constant {
 	let value: Double
 }
+
+extension Constant : IntegerLiteralConvertible {
+	static func convertFromIntegerLiteral(value: IntegerLiteralType) -> Constant {
+		return Constant(value: Double(value))
+	}
+}
+
+extension Constant : FloatLiteralConvertible {
+	static func convertFromFloatLiteral(value: Double) -> Constant {
+		return Constant(value: value)
+	}
+}
