928043baaf2259290f8b8b4440790988afd4815a
Rob Rix <rob.rix@github.com>
2014-08-01 22:50:39 -0400

    Terms can be composites.

--- Loyalist/Term.swift
+++ Loyalist/Term.swift
@@ -1,8 +1,9 @@
 //  Copyright (c) 2014 Rob Rix. All rights reserved.
 
 public enum Term<Constant, Variable> {
+	typealias Recur = Box<Term<Constant, Variable>>
 	case Coefficient(Box<Constant>)
 	case Indeterminate(Box<Variable>)
-	case Addition
-	case Multiplication
+	case Addition(Recur, Recur)
+	case Multiplication(Recur, Recur)
 }
