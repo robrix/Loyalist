0a9f4ebefbbe5a1683e6693fa21665ef7d7940b0
Rob Rix <rob.rix@github.com>
2014-08-02 00:19:38 -0400

    Perform constant folding at construction time.

--- Loyalist/Operators.swift
+++ Loyalist/Operators.swift
@@ -1,9 +1,9 @@
 //  Copyright (c) 2014 Rob Rix. All rights reserved.
 
 func * <Constant : Ring, Variable> (x: Term<Constant, Variable>, y: Term<Constant, Variable>) -> Term<Constant, Variable> {
-	return Term(multiplication: x, y)
+	return Term(multiplication: x, y).simplify()
 }
 
 func + <Constant : Ring, Variable> (x: Term<Constant, Variable>, y: Term<Constant, Variable>) -> Term<Constant, Variable> {
-	return Term(addition: x, y)
+	return Term(addition: x, y).simplify()
 }
