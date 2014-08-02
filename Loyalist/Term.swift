ef2f517c02597d6eed3ceabddc424439d10be001
Rob Rix <rob.rix@github.com>
2014-08-01 23:27:04 -0400

    Add boxing convenience constructors.

--- Loyalist/Term.swift
+++ Loyalist/Term.swift
@@ -6,4 +6,20 @@ public enum Term<Constant : Ring, Variable> {
 	case Indeterminate(Box<Variable>)
 	case Addition(Box<Recur>, Box<Recur>)
 	case Multiplication(Box<Recur>, Box<Recur>)
+	
+	init(constant: Constant) {
+		self = .Coefficient(Box(constant))
+	}
+	
+	init(variable: Variable) {
+		self = .Indeterminate(Box(variable))
+	}
+	
+	init(addition x: Recur, _ y: Recur) {
+		self = .Addition(Box(x), Box(y))
+	}
+	
+	init(multiplication x: Recur, _ y: Recur) {
+		self = .Multiplication(Box(x), Box(y))
+	}
 }
