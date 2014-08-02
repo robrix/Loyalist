cf1a7a1a1bff287f064f48341dfc4cf68e94a492
Rob Rix <rob.rix@github.com>
2014-08-01 20:36:08 -0400

    Initial Commit

---
+++ Loyalist/Loyalist.h
@@ -0,0 +1,19 @@
+//
+//  Loyalist.h
+//  Loyalist
+//
+//  Created by Rob Rix on 2014-08-01.
+//  Copyright (c) 2014 Rob Rix. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Loyalist.
+FOUNDATION_EXPORT double LoyalistVersionNumber;
+
+//! Project version string for Loyalist.
+FOUNDATION_EXPORT const unsigned char LoyalistVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Loyalist/PublicHeader.h>
+
+
