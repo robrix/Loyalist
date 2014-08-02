3cdaa194ed2d02c5cd8db68a8e9cac658ab0e25f
Rob Rix <rob.rix@github.com>
2014-08-01 21:36:45 -0400

    Tidy the tests.

--- LoyalistTests/LoyalistTests.swift
+++ LoyalistTests/LoyalistTests.swift
@@ -1,36 +1,6 @@
-//
-//  LoyalistTests.swift
-//  LoyalistTests
-//
-//  Created by Rob Rix on 2014-08-01.
 //  Copyright (c) 2014 Rob Rix. All rights reserved.
-//
 
 import Cocoa
 import XCTest
 
-class LoyalistTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock() {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
-}
+class LoyalistTests: XCTestCase {}
