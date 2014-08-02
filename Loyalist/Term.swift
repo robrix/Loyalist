808f63310711cad74f33f60b9cdd058317ea8cbd
Rob Rix <rob.rix@github.com>
2014-08-01 22:50:28 -0400

    Term is public.

--- Loyalist/Term.swift
+++ Loyalist/Term.swift
@@ -1,6 +1,6 @@
 //  Copyright (c) 2014 Rob Rix. All rights reserved.
 
-enum Term<Constant, Variable> {
+public enum Term<Constant, Variable> {
 	case Coefficient(Box<Constant>)
 	case Indeterminate(Box<Variable>)
 	case Addition
