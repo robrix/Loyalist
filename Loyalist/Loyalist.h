0893302384eca0d07ad14188f4bcd9cad901b5bb
Rob Rix <rob.rix@github.com>
2014-08-01 21:36:25 -0400

    Tidy the umbrella header.

--- Loyalist/Loyalist.h
+++ Loyalist/Loyalist.h
@@ -1,19 +1,9 @@
-//
-//  Loyalist.h
-//  Loyalist
-//
-//  Created by Rob Rix on 2014-08-01.
 //  Copyright (c) 2014 Rob Rix. All rights reserved.
-//
 
-#import <Cocoa/Cocoa.h>
+@import AppKit.NSLayoutConstraint;
 
 //! Project version number for Loyalist.
-FOUNDATION_EXPORT double LoyalistVersionNumber;
+extern double LoyalistVersionNumber;
 
 //! Project version string for Loyalist.
-FOUNDATION_EXPORT const unsigned char LoyalistVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <Loyalist/PublicHeader.h>
-
-
+extern const unsigned char LoyalistVersionString[];
