5307794d62b771b197a6132bebc6566975bc2b27
Rob Rix <rob.rix@github.com>
2014-08-02 00:08:45 -0400

    Rings have additive & multiplicative identities.

--- Loyalist/Ring.swift
+++ Loyalist/Ring.swift
@@ -1,6 +1,9 @@
 //  Copyright (c) 2014 Rob Rix. All rights reserved.
 
 public protocol Ring {
+	class var additiveIdentity: Self { get }
 	func + (Self, Self) -> Self
+	
+	class var multiplicativeIdentity: Self { get }
 	func * (Self, Self) -> Self
 }
