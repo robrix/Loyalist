4c2f7af3d9bc0e9f7734f3e35324e71ecf1708bf
Rob Rix <rob.rix@github.com>
2014-08-02 00:25:19 -0400

    CGFloats form a Ring.

---
+++ Loyalist/CGFloat+Ring.swift
@@ -0,0 +1,6 @@
+//  Copyright (c) 2014 Rob Rix. All rights reserved.
+
+extension CGFloat : Ring {
+	public static var additiveIdentity: CGFloat { return 0 }
+	public static var multiplicativeIdentity: CGFloat { return 1 }
+}
